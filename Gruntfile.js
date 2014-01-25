92e0853c2946697e50cf865a64dcc9bf001286f5
Kevin Scott <kevin.scott@venmo.com>
2014-01-24 22:51:50 -0500

    Trying to get grunt working

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,43 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    jshint: {
+      options: {
+        eqeqeq: true,
+        trailing: true
+      },
+      target: {
+        src : ['js/**/*.js']
+      }
+    },
+    concat: {
+      options: {
+        
+      },
+      target : {
+        src : [
+          'js/lib/jquery/jquery.min.js',
+          'js/lib/jquery-easing/jquery.easing.min.js',
+          'js/lib/underscore/underscore-min.js',
+          'js/lib/backbone/backbone.js',
+          'js/lib/requirejs-text/text.js',
+          'js/lib/fastclick/lib/fastclick.js',
+          'js/lib/q/q.js',
+          'js/lib/parse-1.2.16.js',
+          'js/app/**/*.js'
+        ],
+        dest : 'js/app.js'
 
+      }
+    },
+    uglify: {
+      options: {
+      },
+      target : {
+        src : ['js/app.js'],
+        dest : 'js/app.min.js'
+      }
+    },
     less: {
       development: {
         options: {
@@ -20,23 +56,80 @@ module.exports = function(grunt) {
     cssmin: {
       compress: {
         files: {
-          'css/style.css': ['css/style.css']
+          'css/style.min.css': ['css/style.css']
         }
       }
     },
     watch: {
+      scripts: {
+        files: ['js/**/*.js','Gruntfile.js'],
+      },
       less: {
         files: 'less/**/*.less',
         tasks: ['less'],
       }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: "./",
+          mainConfigFile: "js/main.js",
+          // optimize: "uglify",
+          // name: "js/lib/almond/almond.js", // assumes a production build using almond
+          modules: [
+          {
+              name: "js/main.js",
+          }],
+          dir: 'build',
+
+          // out: "js/app.min.js",
+          uglify: {
+              toplevel: true,
+              ascii_only: true,
+              beautify: true,
+              max_line_length: 1000,
+
+              //How to pass uglifyjs defined symbols for AST symbol replacement,
+              //see "defines" options for ast_mangle in the uglifys docs.
+              defines: {
+                  DEBUG: ['name', 'false']
+              },
+
+              //Custom value supported by r.js but done differently
+              //in uglifyjs directly:
+              //Skip the processor.ast_mangle() part of the uglify call (r.js 2.0.5+)
+              no_mangle: true
+          },
+
+          // done: function(done, output) {
+          //   var duplicates = require('rjs-build-analysis').duplicates(output);
+
+          //   if (duplicates.length > 0) {
+          //     grunt.log.subhead('Duplicates found in requirejs build:');
+          //     grunt.log.warn(duplicates);
+          //     done(new Error('r.js built duplicate modules, please check the excludes option.'));
+          //   }
+
+          //   done();
+          // }
+        }
+      }
     }
   });
-
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-  grunt.registerTask('deploy', ['less','cssmin']);
-  grunt.registerTask('default', ['less','cssmin','watch']);
+
+
+  // grunt.registerTask('deploy', ['less','cssmin']);
+  grunt.registerTask('default', ['requirejs','less','cssmin']);
+  
+  
+
 
 };
