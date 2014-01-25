2db8a35904998379fc884d752c06e76e7d602b72
Kevin Scott <kevin.scott@venmo.com>
2014-01-24 23:28:53 -0500

    Fix jshint formatting issues

--- js/router.js
+++ js/router.js
@@ -1,26 +1,25 @@
-// Filename: router.js
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'base'
+	'jquery',
+	'underscore',
+	'backbone',
+	'base'
 ], function($, _, Backbone, Base){
 
-  var base = new Base();
-  var AppRouter = Backbone.Router.extend({
-    routes: {
-      "":function(params){ base.index();},
-      "*actions":function(params){ base.fourohfour();},
-    }
-  });
+	var base = new Base();
+	var AppRouter = Backbone.Router.extend({
+		routes: {
+			"":function(params){ base.index();},
+			"*actions":function(params){ base.fourohfour();},
+		}
+	});
 
-  var initialize = function(){
-    var app_router = new AppRouter();
-    base.router = app_router;
-    Backbone.history.start();
-  };
+	var initialize = function(){
+		var app_router = new AppRouter();
+		base.router = app_router;
+		Backbone.history.start();
+	};
 
-  return {
-    initialize: initialize
-  };
+	return {
+		initialize: initialize
+	};
 });
